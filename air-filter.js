b78b38bdf135f098d578e055726c9317f31afe17
shaonianzhentan <ilovevar@gmail.com>
2019-07-29 23:00:11 +0800

    修复在部分浏览器中无法显示的问题

--- air-filter.js
+++ air-filter.js
@@ -7178,7 +7178,7 @@ class AirFilter extends HTMLElement {
       try {
 
 
-        let root = this.createShadowRoot();
+        let root = this.attachShadow({ mode:'open' });
 
         const card = document.createElement('ha-card');
         card.className = 'air-filter'
